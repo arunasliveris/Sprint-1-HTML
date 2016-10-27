d39d742254f873198a807efd7f67087e1d4f0439
Arunas <liverisarunas1@gmail.com>
2016-10-27 21:59:00 +0100

    Minor changes

--- scripts/script.js
+++ scripts/script.js
@@ -1,53 +1,5 @@
-        var button = document.getElementById('button');
-        var body = document.body;
-
-        function init() 
-        {
-            if ( arguments.callee.done )
-                return;
-            arguments.callee.done = true;
-
-            if ( _timer )
-                clearInterval( _timer );
-            body.className = body.className == 'hide' ? '' : 'hide'; 
-        };
-
-        /**
-         * for Mozilla/Opera9
-         */
-        if ( document.addEventListener ) 
-        {
-            document.addEventListener( "DOMContentLoaded", init, false );
-        }
-
-        /**
-         * for Internet Explorer
-         */
-        /*@cc_on @*/
-        /*@if (@_win32)
-            document.write("<script id=__ie_onload defer src=javascript:void(0)><\/script>");
-            var script = document.getElementById("__ie_onload");
-            script.onreadystatechange = function() {
-                if (this.readyState == "complete") {
-                    init(); // call the onload handler
-                }
-            };
-        /*@end @*/
-
-        /**
-         * for Safari
-         */
-        if ( /WebKit/i.test( navigator.userAgent ) )
-        {
-            var _timer = setInterval( function() 
-            {
-                if ( /loaded|complete/.test( document.readyState ) ) 
-                {
-                    init();
-                }
-            }, 10);
-        }
-
-        button.onclick = function() {
-            body.className = body.className == 'hide' ? '' : 'hide'; 
-        }
+   $(document).ready(function(){
+      $('#show_image').on("click", function(){
+         $('#my_images').toggle();
+      });
+   });
